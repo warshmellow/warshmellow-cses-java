0653f902de97fd9a00f1b35ee0f073c06b4ca600
warshmellow <ddwchensf@gmail.com>
2020-07-16 14:51:45 -0700

    update MissingNumber

--- src/main/java/MissingNumber.java
+++ src/main/java/MissingNumber.java
@@ -1,71 +1,68 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class MissingNumber {
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(1, in, out);
+        out.close();
+    }
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
+    static class Task {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+
+            List<Integer> nums = new ArrayList<>();
+
+            for (int i = 0; i < n - 1; i++) {
+                int s = in.nextInt();
+                nums.add(s);
+            }
+
+            out.println(f(nums));
         }
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
+        private static Integer f(List<Integer> nums) {
+            Collections.sort(nums);
+            for (int i = 0; i < nums.size(); i++) {
+                int j = i + 1;
+                if (j != nums.get(i)) {
+                    return j;
                 }
             }
-            return st.nextToken();
+            return nums.size() + 1;
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
         }
 
-        int nextInt() {
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
             return Integer.parseInt(next());
         }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
-
-    private static Integer f(List<Integer> nums) {
-        Collections.sort(nums);
-        for (int i = 0; i < nums.size(); i++) {
-            int j = i + 1;
-            if (j != nums.get(i)) {
-                return j;
-            }
-        }
-        return nums.size() + 1;
-    }
-
-    public static void main(String[] args) {
-        FastReader input = new FastReader();
-        int n = input.nextInt();
-
-        List<Integer> nums = new ArrayList<>();
-
-        for (int i = 0; i < n - 1; i++) {
-            int s = input.nextInt();
-            nums.add(s);
-        }
-
-        System.out.println(f(nums));
     }
 }
